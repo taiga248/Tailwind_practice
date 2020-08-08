2b8c331017b654217840d1a7e75a779cf1bee787
Taiga <taiga248@gmail.com>
2020-08-08 23:31:59 +0900

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // set custom colors
+        systemBlue: "#0d84ff",
+        systemRed: "#FF385F"
+      },
+      fontFamily: {
+        // set google font
+        body: ["Antic Slab"]
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
